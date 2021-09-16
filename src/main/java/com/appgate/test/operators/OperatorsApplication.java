6777aa9aabc68271553bd72ae2e8fad1950c6f45
Eder Navarro <eder.navarrom@perficient.com>
2021-09-15 21:34:44 -0500

    Configuracion Inicial Proyecto: Gradle, Spring Boot

---
+++ src/main/java/com/appgate/test/operators/OperatorsApplication.java
@@ -0,0 +1,13 @@
+package com.appgate.test.operators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperatorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperatorsApplication.class, args);
+	}
+
+}
